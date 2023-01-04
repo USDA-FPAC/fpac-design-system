d68e276009b4844da1324ad93a19880d6bad411e
Jamie Thompson <jamieraethompson@gmail.com>
2023-01-03 18:06:36 -0600

    set up app-icon component [initial #6]

--- src/_partials/BaseHeader.vue
+++ src/_partials/BaseHeader.vue
@@ -25,7 +25,8 @@
       @emitSearch="submitSearch"
       :USE_SEARCH="USE_SEARCH"
     ></globalNav>
-    
+
+    <appIcon name="fds-dashboard"></appIcon>
   </header>
 </template>
 
@@ -40,13 +41,15 @@ import { useNavigation } from "@/_composables/useNavigation";
 import tophat from '@/_components/tophat/tophat.vue';
 import headerApp from '@/_components/header-app/header-app.vue';
 import globalNav from '@/_components/global-nav/global-nav.vue';
+import appIcon from '@/_components/app-icon/app-icon.vue';
 //import modal from '@/_components/Modal.vue';
 
 export default {
   components: {
     tophat,
     headerApp,
-    globalNav
+    globalNav,
+    appIcon
   },
   props: {
     USE_SEARCH: String
