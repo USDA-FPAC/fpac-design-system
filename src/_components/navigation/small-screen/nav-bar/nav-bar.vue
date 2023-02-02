dfd4330ef1065306754bd3ce90887425e1bdec77
Jamie Thompson <jamieraethompson@gmail.com>
2023-02-02 16:34:38 -0600

    updated nav bar to use ds-namespacing [initial #22]

--- src/_components/navigation/small-screen/nav-bar/nav-bar.vue
+++ src/_components/navigation/small-screen/nav-bar/nav-bar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fds-nav-bar fds-hide@l">
+  <div class="ds-nav-bar fds-hide@l">
     <button type="button" class="fds-btn fds-btn--plain ds-btn--icon" title="Open Menu" aria-label="Open Menu">
       <svg class="fds-icon fds-icon--size-2" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path d="M3 18H21V16H3V18ZM3 13H21V11H3V13ZM3 6V8H21V6H3Z" />
@@ -25,7 +25,7 @@ export default {
 <style lang="scss" scoped>
 @use "../../../../_style/core/_fds.variables.scss" as fds;
 
-.fds-nav-bar {
+.ds-nav-bar {
   background-color: fds.$color-white;
   border-bottom: 1px solid fds.$color-fds-tertiary-200;
   position: fixed;
