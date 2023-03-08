a1a483b3b8d5d3c2da9bc887f9596edda8735e06
Casey Hill <bignamehere@gmail.com>
2023-03-08 17:30:38 -0600

    Updates

---
+++ assets/Visual-Style.faf23050.js
@@ -0,0 +1,27 @@
+import{_ as m,d as a,f as n,r as o,u as d,o as c,g as e,h,a as _,e as t,j as b}from"./index.56b436c9.js";import{u}from"./useNavigation.540257b3.js";const x=a(()=>n(()=>import("./BaseHeader.5f5de465.js"),["assets/BaseHeader.5f5de465.js","assets/index.56b436c9.js","assets/index.069ac69f.css","assets/useNavigation.540257b3.js"])),g=a(()=>n(()=>import("./BaseFooter.584b7c30.js"),["assets/BaseFooter.584b7c30.js","assets/index.56b436c9.js","assets/index.069ac69f.css"])),f=a(()=>n(()=>import("./SyntaxHighlight.1583bc06.js"),["assets/SyntaxHighlight.1583bc06.js","assets/index.56b436c9.js","assets/index.069ac69f.css","assets/SyntaxHighlight.64f72b0b.css"])),T={components:{baseHeader:x,baseFooter:g,syntaxHighlight:f},setup(l){o("/fpac-design-system/"),d();const{goto:s}=u();return c(()=>{}),{goto:s}}},v=b(`<main id="main-content" tabindex="-1"><div class="ds-body"><div class="ds-page-title"><div class="ds-page-title__hd"><h1 class="ds-page-title__hd-text" id="article-title">Visual Style</h1></div><div class="ds-page-title__ft"></div></div><div class="ds-l-page"><p class="fds-text--lead fds-m-t--m"> Core building blocks of the Design System to ensure cohesive experiences across all FPAC digital products. </p><div class="ds-l-page__body"><article class="ds-article"><p><code>fds-style</code>, the Design System\u2019s CSS Framework, offers a clean, modern aesthetic that meets high standards of visual accessibility while promoting a sense of credibility, trust, and warmth in the design. </p><h2 id="general">Replace Me</h2><div class="ds-code-example"><pre>                  <code class="ds-code-example__code language-html">\r
+                    <p>The <a href="/link.html">a element</a> and <a class="usa-external_link" href="http://www.kansascity.com" target="_blank">external a element</a></p>\r
+                    <p>The <abbr>abbr element</abbr> and an <abbr title="Abbreviation">abbr</abbr> element with title examples</p>\r
+                    <p>The <b>b element</b> example, though you probably want to use <strong>strong</strong></p>\r
+                    <p>The <cite>cite element</cite> example</p>\r
+                    <p>The <code>code element</code> example</p>\r
+                    <p>The <del>del element</del> example</p>\r
+                    <p>The <dfn>dfn element</dfn> and <dfn title="Title text">dfn element with title</dfn> examples</p>\r
+                    <p>The <em>em element</em> example</p>\r
+                    <p>The <i>i element</i> example, though you probably want to use <em>em</em></p>\r
+                    <p>The <ins>ins element</ins> example</p>\r
+                    <p>The <kbd>kbd element</kbd> example</p>\r
+                    <p>The <mark>mark element</mark> example</p>\r
+                    <p>The <q>q element</q> example</p>\r
+                    <p>The <q>q element <q>inside</q> a q element</q> example</p>\r
+                    <p>The <s>s element</s> example</p>\r
+                    <p>The <samp>samp element</samp> example</p>\r
+                    <p>The <small>small element</small> example</p>\r
+                    <p>The <span>span element</span> example</p>\r
+                    <p>The <strong>strong element</strong> example</p>\r
+                    <p>The <sub>sub element</sub> example</p>\r
+                    <p>The <sup>sup element</sup> example</p>\r
+                    <p>The <time datetime="2005-05-15 19:00">time element</time> example</p>\r
+                    <p>The <u>u element</u> example</p>\r
+                    <p>The <var>var element</var> example</p>\r
+                  </code>\r
+                </pre></div></article></div></div></div></main>`,1);function y(l,s,k,w,S,q){const p=e("baseHeader"),r=e("syntaxHighlight"),i=e("baseFooter");return h(),_("div",null,[t(p,{USE_SEARCH:"true"}),t(r),v,t(i)])}const H=m(T,[["render",y]]);export{H as default};
